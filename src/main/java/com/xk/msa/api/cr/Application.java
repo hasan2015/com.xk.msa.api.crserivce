346d7a7d8d8bbfcb9dea1461dd6f110cca68705d
yanhaixun <yanhaixun@360ser.com>
2017-03-15 16:33:56 +0800

    父级maven工程ok

--- src/main/java/com/xk/msa/api/cr/Application.java
+++ src/main/java/com/xk/msa/api/cr/Application.java
@@ -10,18 +10,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author yanhaixun 2017年1月8日
+ * 
+ * @author yanhaixun
+ * @date 2017年3月14日 下午8:00:12
+ *
  */
 @RestController
 @SpringBootApplication(scanBasePackages = "com.xk.msa")
-public class SampleApplication {
+public class Application {
 	@RequestMapping(name = "HelloService", method = RequestMethod.GET, path = "/hello")
 	public String hello() {
 		return "hello";
 	}
  
 	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
